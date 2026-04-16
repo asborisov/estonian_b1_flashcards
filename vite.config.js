8cb0ba7ef153714784e6b64420129e803c206095
Artem Borisov <timan.b@pm.me>
2026-04-16 17:30:43 +0300

    Fixing runtime error

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
-import path from 'node:path';
-import { createRequire } from 'node:module';
 import { defineConfig } from 'vite';
-
-const require = createRequire(import.meta.url);
-const solidPluginPath = require.resolve('vite-plugin-solid');
-const solidPackagePath = require.resolve('solid-js/package.json');
-const solidWebPath = require.resolve('solid-js/web');
-const solid = (await import(solidPluginPath)).default;
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   base: '/estonian_b1_flashcards/',
   plugins: [solid()],
-  resolve: {
-    alias: {
-      'solid-js/web': solidWebPath,
-      'solid-js': path.dirname(solidPackagePath),
-    },
-  },
   build: {
     target: 'esnext',
   },
